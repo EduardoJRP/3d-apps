ff93e2d656ea72f32173e826a35be2bd28423e42
EduardoJRP <99346540+EduardoJRP@users.noreply.github.com>
2023-03-18 11:20:57 -0500

    Added list on the Todo list page

--- to_do_list/src/App.js
+++ to_do_list/src/App.js
@@ -1,4 +1,4 @@
-import React, { userState } from "react";
+import React, { useState } from "react";
 import './App.css';
 
 // Importing Components
@@ -6,12 +6,15 @@ import Form from "./components/Form"
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header>
         <h1>Hello World</h1>
       </header>
-      <Form />
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
       <TodoList />
     </div>
   );
