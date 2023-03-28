e3b8f0712b19bdf47ab845e10de0aa7e448e08b4
EduardoJRP <99346540+EduardoJRP@users.noreply.github.com>
2023-03-27 23:24:48 -0500

    Added routes to my web app.

--- react_fullstack/client/src/App.js
+++ react_fullstack/client/src/App.js
@@ -1,25 +1,16 @@
 import './App.css';
-import axios from 'axios';
-import {useEffect, useState} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./pages/Home"
 
 function App() {
-
-  const [listOfPosts, setListOfPosts] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:3001/posts").then((response) => {
-      setListOfPosts(response.data);
-    })
-  }, [])
-
   return (
-    <div className = "App">{listOfPosts.map((value, key) => {
-      return <div className="post">
-        <div className='title'> {value.title} </div>
-        <div className='body'> {value.postText} </div>
-        <div className='footer'> {value.username} </div>
-      </div>
-    })}</div>
+    <div className = "App">
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
