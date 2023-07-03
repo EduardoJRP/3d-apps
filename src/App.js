f0fc2f0327d2d99855bb2067da5b83f508cc9250
EduardoJRP <99346540+EduardoJRP@users.noreply.github.com>
2023-07-03 18:54:47 -0500

    Quick tutorial of three.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import Cylinder3d from "./components/Cylinder3d";
+ 
+function App() {
+  return (
+    <>
+      <section className='App-header'>
+        <Canvas>
+          <pointLight position={[10, 10, 10]} />
+          {/* <pointLight position={[10, 10, 10]} /> */}
+          <ambientLight />
+          <Cylinder3d position={[-1.2, 0, 0]} />
+          <Cylinder3d position={[1.2, 0, 0]} />
+        </Canvas>
+      </section>
+    </>
+  );
+}
+ 
+export default App;
