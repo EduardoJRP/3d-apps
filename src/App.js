dee96d21900c7a176450a801ca9830e45faf3b03
EduardoJRP <99346540+EduardoJRP@users.noreply.github.com>
2023-07-05 18:03:50 -0500

    Organized Project

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import Cylinder3d from "./components/Cylinder3d";
  
 function App() {
   return (
-    <>
-      <section className='App-header'>
-        <Canvas>
-          <pointLight position={[10, 10, 10]} />
-          {/* <pointLight position={[10, 10, 10]} /> */}
-          <ambientLight />
-          <Cylinder3d position={[-1.2, 0, 0]} />
-          <Cylinder3d position={[1.2, 0, 0]} />
-        </Canvas>
-      </section>
-    </>
+    <section className='App-header'>
+      <h1>Hello World!</h1>
+      <Canvas>
+        <pointLight position={[10, 10, 10]} />
+        {/* <pointLight position={[10, 10, 10]} /> */}
+        <ambientLight />
+        <Cylinder3d position={[-1.2, 0, 0]} />
+        <Cylinder3d position={[1.2, 0, 0]} />
+      </Canvas>
+    </section>
   );
 }
  
